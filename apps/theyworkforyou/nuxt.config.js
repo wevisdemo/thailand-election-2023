b0871bbfa292780c40e0ee7542df7fdb5f5f0ca0
Naanong (Key) <naanong@punchup.world>
2023-03-07 21:47:41 +0700

    feat(theyworkforyou): add styles package

--- apps/theyworkforyou/nuxt.config.js
+++ apps/theyworkforyou/nuxt.config.js
@@ -27,7 +27,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['element-ui/lib/theme-chalk/index.css'],
+  css: ['element-ui/lib/theme-chalk/index.css', '~/assets/styles/index.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: ['@/plugins/element-ui'],
