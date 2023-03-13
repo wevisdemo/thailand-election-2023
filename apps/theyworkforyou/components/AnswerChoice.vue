138546aab2e32fbbb60816ddc903126eab2efb32
Naanong (Key) <naanong@punchup.world>
2023-03-13 12:49:36 +0700

    feat(theyworkforyou): create answer choice

---
+++ apps/theyworkforyou/components/AnswerChoice.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="choice-btn">
+    <div class="vote-icon">
+      <img :src="getVoteIcon" :alt="choice_id" />
+    </div>
+    <p class="typo-b4">
+      <b>{{ text }}</b>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      default: '',
+    },
+    choice_id: {
+      type: String,
+      default: '',
+    },
+  },
+  computed: {
+    getVoteIcon() {
+      return require(`~/assets/images/icons/${this.choice_id}.svg`)
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.choice-btn {
+  display: flex;
+  align-items: center;
+  padding: 13px 18px;
+  background: var(--color-white);
+  border: 3px solid var(--color-black);
+  border-radius: 30px;
+  cursor: pointer;
+}
+.vote-icon {
+  width: 20px;
+  margin-right: 5px;
+}
+</style>
