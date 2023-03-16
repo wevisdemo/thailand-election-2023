b1ef4fb3706c62ba0a56500a1900eaa143bb6b0a
Th1nkK1D <witheep@gmail.com>
2023-03-16 12:07:46 +0200

    feat(components): add election-bottom

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,5 +1,7 @@
 /* @refresh reload */
 import { customElement } from 'solid-element';
+import Bottom from './bottom/bottom';
 import Footer from './footer/footer';
 
+customElement('election-bottom', Bottom);
 customElement('election-footer', Footer);
