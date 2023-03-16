371e49ef2924ea8fe654ded2abe4e9f9de51cbeb
Th1nkK1D <witheep@gmail.com>
2023-03-16 14:30:53 +0200

    feat(components): add election-header

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -2,6 +2,8 @@
 import { customElement } from 'solid-element';
 import Bottom from './bottom/bottom';
 import Footer from './footer/footer';
+import Header from './header/header';
 
+customElement('election-header', Header);
 customElement('election-bottom', Bottom);
 customElement('election-footer', Footer);
