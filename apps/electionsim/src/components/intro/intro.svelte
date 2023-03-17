394d5dd88f633217b64521c8047249459cdcc75a
tesol2y090 <gliese090@gmail.com>
2023-03-17 15:48:06 +0700

    fix(electionsim): delete share components in intro

--- apps/electionsim/src/components/intro/intro.svelte
+++ apps/electionsim/src/components/intro/intro.svelte
@@ -1,14 +1,11 @@
 <script lang="ts">
 	import byxMobileLogo from '../../images/byx-mobile.svg';
 	import simulatorBx from '../../images/simulator_bx.png';
-	import fbLogo from '../../images/facebook.png';
-	import twLogo from '../../images/twitter.png';
-	import lineLogo from '../../images/line.png';
 </script>
 
 <div class="flex flex-col items-center w-[650px] py-10">
 	<h7 class="typo-h7">Election Simulator</h7>
-	<h2 class="typo-h2 beyonx-text-gradient bold text-center">
+	<h2 class="typo-h2 beyonx-text-gradient text-center font-bold">
 		ทดลองเป็นนัก วิเคราะห์ #เลือกตั้ง66
 	</h2>
 	<div class="flex items-center mt-1 mb-5">
@@ -22,21 +19,9 @@
 		แล้ววิเคราะห์ความเป็นไปได้ต่างๆ ของการ เลือกตั้งครั้งนี้ไปด้วยกัน
 	</p>
 	<button
-		class="beyondx-gradient-bg text-white py-2 px-4 w-52 flex items-center justify-between"
+		class="beyondx-gradient-bg text-white py-2 px-4 w-52 flex items-center justify-between font-bold"
 	>
 		เริ่มเลย
 		<div class="border-1 border-t border-r w-2 h-2 rotate-45" />
 	</button>
 </div>
-
-<div class="flex flex-row items-center">
-	<span class="typo-b6">Share</span>
-	<img class="mx-1" src={fbLogo} alt="facebook" />
-	<img class="mx-1" src={twLogo} alt="twitter" />
-	<img class="mx-1" src={lineLogo} alt="line" />
-</div>
-<h3 class="typo-b3 my-2">#WeVisElection66</h3>
-<button
-	class="py-2 px-4 border border-[3px] hover:border-highlight-2 rounded-full bold"
-	>เกี่ยวกับโครงการ</button
->
