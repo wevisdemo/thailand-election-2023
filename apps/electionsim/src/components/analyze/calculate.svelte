00fc7c2fa63ac513b718e1bcb3a8ba83f206fed6
tesol2y090 <gliese090@gmail.com>
2023-03-19 23:50:22 +0700

    feat(electionsim): add analyze page

---
+++ apps/electionsim/src/components/analyze/calculate.svelte
@@ -0,0 +1,41 @@
+<div
+	class="text-center flex flex-col items-center justify-center w-[312px] md:w-[650px] h-full py-10"
+>
+	<svg
+		width="69"
+		height="69"
+		viewBox="0 0 69 69"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<path
+			d="M66.5138 40.0567C65.4148 46.2896 62.4922 52.0566 58.1156 56.6284C53.7391 61.2003 48.105 64.3716 41.9261 65.7415C35.7471 67.1113 29.3007 66.6181 23.402 64.3243C17.5033 62.0304 12.4173 58.0389 8.78713 52.8544C5.15697 47.67 3.14568 41.5256 3.00762 35.1981C2.86955 28.8706 4.6109 22.6442 8.01147 17.3064C11.412 11.9686 16.3191 7.75902 22.1121 5.21003C27.9051 2.66105 34.3239 1.88713 40.5567 2.98615"
+			stroke="url(#paint0_angular_1691_66021)"
+			stroke-width="5"
+		/>
+		<defs>
+			<radialGradient
+				id="paint0_angular_1691_66021"
+				cx="0"
+				cy="0"
+				r="1"
+				gradientUnits="userSpaceOnUse"
+				gradientTransform="translate(35 34.5) rotate(0.830315) scale(34.5036)"
+			>
+				<stop stop-color="#FF0000" />
+				<stop offset="0.744792" stop-color="#0000FF" />
+			</radialGradient>
+		</defs>
+	</svg>
+	<h6 class="typo-h6 font-bold mt-5">กำลังคำนวณ</h6>
+	<h4 class="typo-b4 mt-1">มาดูแบบจำลองผล #เลือกตั้ง66จากคำตอบของคุณกัน</h4>
+	<div class="w-full bg-gray-4 mt-[180px] text-left">
+		<p class="typo-b6 py-2 px-4">
+			<b>Disclaimer</b>: ผลการวิเคราะห์นี้เป็นเพียงการ ทดลองประมาณการแบบคร่าวๆ
+			บนฐานข้อมูลผลการ
+			เลือกตั้งเดิมบนกติกาเลือกตั้งใหม่และข้อสันนิษฐานที่กำหนดไว้เท่านั้น
+			อาจมีปัจจัยอื่นที่ส่งผลต่อ ผลลัพธ์ในการเลือกตั้ง
+			และไม่ได้มีเจตนาในการชี้นำผลการเลือกตั้งแต่อย่างใด
+		</p>
+	</div>
+</div>
