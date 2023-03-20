aedcd794e746ffcaf4b9d30c59e2a6b1c3afaf58
Th1nkK1D <witheep@gmail.com>
2023-03-20 15:50:58 +0000

    Deploying to gh-pages from @ wevisdemo/thailand-election-2023@5f085fb806c643ebd8fae78100d81663de9a1981 🚀

---
+++ electionsim/_app/immutable/chunks/1.616282dc.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.07b4bd7d.js";export{t as component};
--- electionsim/_app/immutable/chunks/1.cb1a1e8a.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.792f8ece.js";export{t as component};
