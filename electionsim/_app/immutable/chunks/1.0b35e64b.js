c8c16006bb9bc6f149df5acc55b81662f49ab32d
tesol2y090 <gliese090@gmail.com>
2023-03-21 16:30:22 +0000

    Deploying to gh-pages from @ wevisdemo/thailand-election-2023@936e956109a79829e257922602abc6822b11de6e 🚀

--- electionsim/_app/immutable/chunks/1.0b35e64b.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.5d1b306b.js";export{t as component};
---
+++ electionsim/_app/immutable/chunks/2.c038553b.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.2a3e817e.js";export{t as component};
