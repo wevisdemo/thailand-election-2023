b9d2985910b40bb2e158090c5ee1e3b4155b1ff6
Th1nkK1D <witheep@gmail.com>
2023-03-22 20:53:56 +0200

    chore(bundler): support underconstruction page for production

--- bundler/src/index.ts
+++ bundler/src/index.ts
@@ -5,23 +5,35 @@ import projectMetadata from '@thailand-election-2023/metadata/dist/projects.json
 interface ProjectConfig {
 	source: string;
 	destination: string;
+	isUnderConstruction: boolean;
 }
 
 const APPS_DIR = 'apps';
 const BUILD_DIR = 'dist';
+const constructionArtifactPath = join(
+	'../',
+	APPS_DIR,
+	'underconstruction',
+	BUILD_DIR
+);
 
 const subProjects: ProjectConfig[] = [
 	...projectMetadata
 		.filter(({ URL }) => URL.startsWith('/'))
-		.map(({ URL }) => ({
+		.map(({ URL, ComingSoon }) => ({
 			source: URL,
 			destination: URL,
+			isUnderConstruction:
+				process.env.BUILD_TARGET === 'production' && ComingSoon,
 		})),
 	{
 		source: '/landing',
 		destination: '/',
+		isUnderConstruction: false,
 	},
 ];
+
+console.log(`Build target: ${process.env.BUILD_TARGET || 'default'}`);
 
 console.log(`${subProjects.length} sub-projects found.`);
 
@@ -29,20 +41,29 @@ if (existsSync(BUILD_DIR)) {
 	rmSync(BUILD_DIR, { recursive: true });
 }
 
-subProjects.forEach(({ source, destination }) => {
+subProjects.forEach(({ source, destination, isUnderConstruction }) => {
 	const buildArtifactsPath = join('../', APPS_DIR, source, BUILD_DIR);
+	const isBuildArtifactExist = existsSync(buildArtifactsPath);
 
-	if (existsSync(buildArtifactsPath)) {
-		console.log(`Copying ${source}...`);
-
-		cpSync(buildArtifactsPath, join(BUILD_DIR, destination), {
-			recursive: true,
-		});
-	} else {
+	if (isUnderConstruction) {
+		console.log(`${source} is coming soon. Copying underconstruction page...`);
+	} else if (!isBuildArtifactExist) {
 		console.warn(
-			`[Warning] ${source} built artifacts is not found at ${buildArtifactsPath}`
+			`${source} built artifacts is not found at ${buildArtifactsPath}. Copying underconstruction page...`
 		);
+	} else {
+		console.log(`${source} found! Copying...`);
 	}
+
+	cpSync(
+		isUnderConstruction || !isBuildArtifactExist
+			? constructionArtifactPath
+			: buildArtifactsPath,
+		join(BUILD_DIR, destination),
+		{
+			recursive: true,
+		}
+	);
 });
 
-console.log('Completed');
+console.log('Completed!');
