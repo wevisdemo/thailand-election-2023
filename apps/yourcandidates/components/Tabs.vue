0aa9b049cef5aa5c10343193d7f4183d2a47d7a6
napatswift <napat.do@ku.th>
2023-03-23 14:39:38 +0700

    feat(yourcandidates): Add tabs comp

---
+++ apps/yourcandidates/components/Tabs.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="tabs">
+    <div class="tabs-nav">
+      <ul>
+        <li
+          v-for="(tab, index) in tabs"
+          :key="index"
+          :class="{ active: activeTab === index }"
+          @click="activeTab = index"
+        >
+          {{ tab.label }}
+        </li>
+      </ul>
+    </div>
+    <div class="tabs-content">
+      <slot :name="tabs[activeTab].name"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Tabs',
+  props: {
+    tabs: {
+      type: Array,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      activeTab: 0,
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.tabs {
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  justify-content: stretch;
+}
+
+.tabs-nav {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+}
+
+.tabs-nav ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  width: 100%;
+}
+
+.tabs-nav li {
+  font-weight: 700;
+  padding: 10px;
+  cursor: pointer;
+  border-width: 0px 0px 3px 0px;
+  border-color: transparent transparent var(--color-black) transparent;
+  border-radius: 0px;
+  flex-grow: 1;
+  text-align: center;
+}
+
+.tabs-nav li.active {
+  border-width: 3px 3px 0px 3px;
+  border-style: solid;
+  border-color: var(--color-black);
+  border-radius: 10px 10px 0px 0px;
+}
+
+.tabs-content {
+  flex: 1;
+  border-width: 0px 3px 3px 3px;
+  border-style: solid;
+  border-color: var(--color-black);
+}
+</style>
--- apps/yourcandidates/pages/_province/_electorate.vue
+++ apps/yourcandidates/pages/_province/_electorate.vue
@@ -1,194 +1,215 @@
 <template>
-    <div>
-        <election-header></election-header>
-        <div v-show="openPopup" class="popup flex flex-col justify-center items-center relative scrollbar-hid">
-            <div class="relative w-4/5 h-4/5 max-w-650 overflow-y-scroll scrollbar-hide" style="border-radius: 10px;">
-                <VoteInstruction />
-                <div class="absolute top-4 right-8">
-                    <button class="fixed" @click="closePopup()"> <IconsClose/> </button>
-                </div>
-            </div>
+  <div>
+    <election-header></election-header>
+    <div
+      v-show="openPopup"
+      class="popup flex flex-col justify-center items-center relative scrollbar-hid"
+    >
+      <div
+        class="relative w-4/5 h-4/5 max-w-650 overflow-y-scroll scrollbar-hide"
+        style="border-radius: 10px"
+      >
+        <VoteInstruction />
+        <div class="absolute top-4 right-8">
+          <button class="fixed" @click="closePopup()"><IconsClose /></button>
         </div>
-
-        <nuxt-link to="/" class="tab-back">
-            <IconsArrow/>
-            <span class="detail" target="_blank"> 
-                กลับไปหน้าค้นหา
-            </span>
-        </nuxt-link>
-        
-        <div class="main-container">
-            <div class="head-section">
-                <div class="electorate-description">
-                    <div class="typo-h3">
-                        <b> รายชื่อผู้สมัคร </b>
-                    </div>
-                    <div class="typo-h7">
-                        <b> {{province}} เขตเลือกตั้งที่ {{electorateNumber}} </b>
-                    </div>
-                    <div v-for="(electorate, i) in electorates" :key="i" style="padding-left: 8px;">
-                        <li>{{electorate}}</li>
-                    </div>
-                </div>       
-                
-                <button class="vode-instruction-section" @click="popupVoteInstruction()">
-                    <div class="detail">
-                        <IconsInformation/>
-                        <b>รอบนี้มีบัตร 2 ใบนะ รู้ยัง?</b>
-                    </div>
-                    <IconsPlus/>
-                </button>
-            </div>
-
-            <!-- tab -->
-            <div class="">
-                <div class="filter">
-
-                </div>
-                <div class="search">
-
-                </div>
-                <div class="candidate-card">
-                    <PeopleCard
-                    v-for="people in peoples"
-                    :key="people.number"
-                    :people="{...people}"
-                    />
-                </div>
-            </div>
-        </div>
-        
-        <election-bottom></election-bottom>
-        <election-footer></election-footer>
+      </div>
     </div>
+
+    <nuxt-link to="/" class="tab-back">
+      <IconsArrow />
+      <span class="detail" target="_blank"> กลับไปหน้าค้นหา </span>
+    </nuxt-link>
+
+    <div class="main-container">
+      <div class="head-section">
+        <div class="electorate-description">
+          <div class="typo-h3">
+            <b> รายชื่อผู้สมัคร </b>
+          </div>
+          <div class="typo-h7">
+            <b> {{ province }} เขตเลือกตั้งที่ {{ electorateNumber }} </b>
+          </div>
+          <div
+            v-for="(electorate, i) in electorates"
+            :key="i"
+            style="padding-left: 8px"
+          >
+            <li>{{ electorate }}</li>
+          </div>
+        </div>
+
+        <button
+          class="vode-instruction-section"
+          @click="popupVoteInstruction()"
+        >
+          <div class="detail">
+            <IconsInformation />
+            <b>รอบนี้มีบัตร 2 ใบนะ รู้ยัง?</b>
+          </div>
+          <IconsPlus />
+        </button>
+      </div>
+
+      <!-- tab -->
+      <Tabs :tabs="tabs">
+        <div slot="tab1">
+          <div class="">
+            <div class="filter">ทั้งหมด 2 คนมั้ง</div>
+            <div class="search">
+              <input type="text" />
+            </div>
+            <div class="candidate-card">
+              <PeopleCard
+                v-for="people in peoples"
+                :key="people.number"
+                :people="{ ...people }"
+              />
+            </div>
+          </div>
+        </div>
+        <div slot="tab2">Tab 2 content goes here</div>
+      </Tabs>
+    </div>
+
+    <election-bottom></election-bottom>
+    <election-footer></election-footer>
+  </div>
 </template>
   
 <script>
-import PeopleCard from '@/components/candidateCard/PeopleCard.vue';
-import { TheyWorkForUs } from '@thailand-election-2023/database';
-import {ElectionHeader, ElectionBottom, ElectionFooter} from '@thailand-election-2023/components';
+import PeopleCard from '@/components/candidateCard/PeopleCard.vue'
+import { TheyWorkForUs } from '@thailand-election-2023/database'
+import {
+  ElectionHeader,
+  ElectionBottom,
+  ElectionFooter,
+} from '@thailand-election-2023/components'
 
 export default {
-    async asyncData({params: {province, electorateNumber}}) {
-        // const candidates = (await TheyWorkForUs.People.fetch()).list;
-        const peoples = [
-            {
-                'number': '1',
-                'imageCandidate' : '',
-                'imageParty': '',
-                'name': 'พิรสุต จันทรานุวัฒน์',
-                'numberPartyGroup': '2',
-                'partyGroup': 'อดีต ส.ส. ทั้งฝ่ายรัฐบาลและฝ่ายค้าน',
-                'party': 'รวมไทยสร้างชาติ',
-                'age': '50',
-                'education': 'ปริญญาตรี',
-                'occupation': 'พนักงานบริษัทเอกชน',
-            },
-            {
-                'number': '2',
-                'imageCandidate' : '',
-                'imageParty': '',
-                'name': 'พิรสุต จันทรานุวัฒน์',
-                'numberPartyGroup': '1',
-                'partyGroup': 'อดีต ส.ส. ฝ่ายค้าน',
-                'party': 'รวมไทยสร้างชาติ',
-                'age': '50',
-                'education': 'ปริญญาตรี',
-                'occupation': 'พนักงานบริษัทเอกชน',
-            }
-        ]
-        return {peoples}
+  async asyncData({ params: { province, electorateNumber } }) {
+    // const candidates = (await TheyWorkForUs.People.fetch()).list;
+    const peoples = [
+      {
+        number: '1',
+        imageCandidate: '',
+        imageParty: '',
+        name: 'พิรสุต จันทรานุวัฒน์',
+        numberPartyGroup: '2',
+        partyGroup: 'อดีต ส.ส. ทั้งฝ่ายรัฐบาลและฝ่ายค้าน',
+        party: 'รวมไทยสร้างชาติ',
+        age: '50',
+        education: 'ปริญญาตรี',
+        occupation: 'พนักงานบริษัทเอกชน',
+      },
+      {
+        number: '2',
+        imageCandidate: '',
+        imageParty: '',
+        name: 'พิรสุต จันทรานุวัฒน์',
+        numberPartyGroup: '1',
+        partyGroup: 'อดีต ส.ส. ฝ่ายค้าน',
+        party: 'รวมไทยสร้างชาติ',
+        age: '50',
+        education: 'ปริญญาตรี',
+        occupation: 'พนักงานบริษัทเอกชน',
+      },
+    ]
+    return { peoples }
+  },
+  data() {
+    return {
+      province: this.$route.params.province,
+      electorateNumber: this.$route.params.electorate,
+      electorates: ['เขตบางบอน', 'เขตหนองแขม เฉพาะ แขวงหนองแขม'],
+      openPopup: false,
+      tabs: [
+        { name: 'tab1', label: '1. เลือกคนที่รัก' },
+        { name: 'tab2', label: '2. เลือกพรรคที่ชอบ' },
+      ],
+    }
+  },
+  components: {
+    PeopleCard,
+    ElectionHeader,
+    ElectionBottom,
+    ElectionFooter,
+  },
+  methods: {
+    popupVoteInstruction() {
+      this.openPopup = true
     },
-    data() {
-        return {
-            province: this.$route.params.province,
-            electorateNumber: this.$route.params.electorate,
-            electorates: ["เขตบางบอน", "เขตหนองแขม เฉพาะ แขวงหนองแขม"],
-            openPopup: false,
-        };
+    closePopup() {
+      this.openPopup = false
     },
-    components: { 
-        PeopleCard, 
-        ElectionHeader, 
-        ElectionBottom, 
-        ElectionFooter 
-    },
-    methods: {
-        popupVoteInstruction() {
-            this.openPopup = true;
-        },
-        closePopup() {
-            this.openPopup = false;
-        }
-    },
+  },
 }
 </script>
 
 <style scoped>
-
 .tab-back {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    padding: 10px;
-    gap: 4px;
-    flex: none;
-    order: 1;
-    flex-grow: 0;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 10px;
+  gap: 4px;
+  flex: none;
+  order: 1;
+  flex-grow: 0;
 }
 
 .tab-back > .detail {
-    flex: none;
-    order: 1;
-    flex-grow: 0;
+  flex: none;
+  order: 1;
+  flex-grow: 0;
 }
 
 .head-section {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    gap: 20px;
-    flex: none;
-    order: 0;
-    flex-grow: 0;
-    padding-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 20px;
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+  padding-bottom: 20px;
 }
 
 .electorate-description {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    padding: 0px;
-    flex: none;
-    order: 0;
-    align-self: stretch;
-    flex-grow: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0px;
+  flex: none;
+  order: 0;
+  align-self: stretch;
+  flex-grow: 0;
 }
 
 .vode-instruction-section {
-    background: var(--color-gray-1);
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-    padding: 10px;
-    gap: 5px;
-    border-radius: 2px;
-    flex: none;
-    order: 1;
-    align-self: stretch;
-    flex-grow: 0;
+  background: var(--color-gray-1);
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+  gap: 5px;
+  border-radius: 2px;
+  flex: none;
+  order: 1;
+  align-self: stretch;
+  flex-grow: 0;
 }
 
 .vode-instruction-section > .detail {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    padding: 0px;
-    gap: 5px;
-    flex: none;
-    order: 0;
-    flex-grow: 0;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 0px;
+  gap: 5px;
+  flex: none;
+  order: 0;
+  flex-grow: 0;
 }
 
 .popup {
@@ -204,20 +225,19 @@ export default {
 }
 
 .scrollbar-hide::-webkit-scrollbar {
-    display: none;
+  display: none;
 }
 
 .scrollbar-hide {
-    -ms-overflow-style: none;
-    scrollbar-width: none;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
 }
 
 .candidate-card {
-    display: flex;
-    flex-direction: column;
-    padding: 0px;
-    gap: 10px;
-    z-index: 0;
+  display: flex;
+  flex-direction: column;
+  padding: 0px;
+  gap: 10px;
+  z-index: 0;
 }
-
 </style>
