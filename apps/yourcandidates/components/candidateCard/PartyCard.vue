e4b60cd5763769de0a5b1adf04fe89f1942b44d2
JaeAiKay <78583537+JaeAiKay@users.noreply.github.com>
2023-03-23 15:27:06 +0700

    feat(yourcandidates): add party list

--- apps/yourcandidates/components/candidateCard/PartyCard.vue
+++ apps/yourcandidates/components/candidateCard/PartyCard.vue
@@ -1,26 +1,20 @@
 <template>
-    <TemplateCard :key="parties.number" :candidate="{...parties}">
+    <TemplateCard :key="party.number" :candidate="{...party}">
         <template v-slot:infomation>
-            <table class="table-container">
-                <tbody>
-                    <tr> 
-                        <td style="color: var(--color-gray-3)">สังกัด</td>
-                        <td class="pl-2">{{ parties.party }}</td>
-                    </tr>
-                    <tr> 
-                        <td style="color: var(--color-gray-3)">อายุ</td>
-                        <td class="pl-2">{{ parties.age }}</td>
-                    </tr>
-                    <tr> 
-                        <td style="color: var(--color-gray-3)">การศึกษา</td>
-                        <td class="pl-2">{{ parties.education }}</td>
-                    </tr>
-                    <tr> 
-                        <td style="color: var(--color-gray-3)">อาชีพเดิม</td>
-                        <td class="pl-2">{{ parties.occupation }}</td>
-                    </tr>
-                </tbody>
-            </table>
+            <div class="candidate-section">
+                <p style="color: var(--color-gray-3)"> แคนเดตนายก </p>
+                <div class="party-list"
+                v-for="primeCandidate in party.partyList">
+                    <div class="image-prime-candidate">
+
+                    </div>
+                    <p> {{ primeCandidate.name }} </p>
+                </div>
+            </div>
+            <button class="party-list-button">
+                <IconsPlus/>
+                <p> ดูบัญชีรายชื่อ </p>
+            </button>
         </template>
     </TemplateCard>     
 </template>
@@ -29,7 +23,7 @@
 import TemplateCard from '@/components/candidateCard/TemplateCard.vue'
 export default{
     props: {
-        parties: {
+        party: {
             type: Object,
             default: {}
         }
@@ -39,3 +33,48 @@ export default{
     }
 }
 </script>
+
+<style scoped>
+.candidate-section{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    padding: 0px;
+    gap: 5px;
+    flex: none;
+    order: 0;
+    flex-grow: 0;
+}
+.party-list{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding: 0px;
+    gap: 5px;
+}
+
+.image-prime-candidate{
+    box-sizing: border-box;
+    background: var(--color-gray-2);
+    border: 1px solid var(--color-black);
+    border-radius: 50px;
+    width: 30px;
+    height: 30px;
+    flex: none;
+    order: 0;
+    flex-grow: 0;
+}
+
+.party-list-button{
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding: 4px 10px;
+    gap: 5px;
+    background: var(--color-gray-2);
+    border: 1px solid var(--color-gray-2);
+    border-radius: 100px;
+}
+</style>
