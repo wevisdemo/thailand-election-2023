e889c4ee218db8f9340b3e1246c97181bc5a29b7
napatswift <napat.do@ku.th>
2023-04-01 18:58:05 +0700

    feat(yourcandidates): Add mock data

--- apps/yourcandidates/helpers/peoplestore.js
+++ apps/yourcandidates/helpers/peoplestore.js
@@ -6,25 +6,32 @@ export const getPeople = async (province, zone) => {
   let peoplesHistory = await TheyWorkForUs.People.fetchAll({
     fields: ['Name', 'PeoplePartyHistory'],
     where: `(MpProvince,eq,${province})~and(MpZone,eq,${zone})~and(MpType,eq,แบ่งเขต)`,
-    'nested[PeoplePartyHistory][nested][Party][fields]': 'Id,Name,PartyGroup'
+    'nested[PeoplePartyHistory][nested][Party][fields]': 'Id,Name,PartyGroup',
   })
 
   let parties = await TheyWorkForUs.Parties.fetchAll({
     fields: ['Id', 'Name', 'Images'],
   })
   // filter candidates by province and zone
-  let peoples = peoples_list 
-  peoples = peoples.filter((people) => people.Province == province && people.Zone == zone)
+  let peoples = peoples_list
+  peoples = peoples.filter(
+    (people) => people.Province == province && people.Zone == zone
+  )
   // left join between candidate peoplePartyHistory
-  peoples = peoples.map((people) => 
-    peoplesHistory.some((history) => people.Name === history.Name)?  
-    Object.assign(people, peoplesHistory.find((history) => history.Name === people.Name))
-    :people
+  peoples = peoples.map((people) =>
+    peoplesHistory.some((history) => people.Name === history.Name)
+      ? Object.assign(
+          people,
+          peoplesHistory.find((history) => history.Name === people.Name)
+        )
+      : people
   )
   // join candidates and party (for use image party)
-  for(let i=0; i < peoples.length; i++){
+  for (let i = 0; i < peoples.length; i++) {
     let partyName = await peoples[i].Party
-    peoples[i]['Party'] = await parties.find((party) => party.Name === partyName)
+    peoples[i]['Party'] = await parties.find(
+      (party) => party.Name === partyName
+    )
   }
 
   return peoples
@@ -33,5 +40,3 @@ export const getPeople = async (province, zone) => {
 export const getParties = async (province, zone) => {
   return parties
 }
-
-
