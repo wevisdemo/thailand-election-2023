8c79fcec9b4f2afc309b1b1fc48d4b2c90bf782f
Th1nkK1D <witheep@gmail.com>
2023-04-03 08:56:35 +0000

    Deploying to gh-pages from @ wevisdemo/thailand-election-2023@52683cfd5397df5e7b04abecf96ee108300bf6e8 🚀

--- theyworkforyou/_nuxt/a351a66.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[18],{436:function(n,o,r){"use strict";var t=r(3),c=r(104).find,d=r(192),e="find",f=!0;e in[]&&Array(1)[e]((function(){f=!1})),t({target:"Array",proto:!0,forced:f},{find:function(n){return c(this,n,arguments.length>1?arguments[1]:void 0)}}),d(e)}}]);
