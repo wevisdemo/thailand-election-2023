43e4c1e6abd383d0e03684f2864f8f7c4b95e5d8
JaeAiKay <78583537+JaeAiKay@users.noreply.github.com>
2023-04-03 22:59:46 +0700

    fix(yourcandidates): edit join candidates with the party

--- apps/yourcandidates/helpers/candidatestore.js
+++ apps/yourcandidates/helpers/candidatestore.js
@@ -1,9 +1,9 @@
 import { TheyWorkForUs } from '@thailand-election-2023/database'
-import peoples_list from '@/data/people_list.json'
-import parties from '@/data/parties.json'
+import people_list from '../data/people_list.json'
+import parties from '../data/parties.json'
 
 export const getPeople = async (province, zone) => {
-  let peoplesHistory = await TheyWorkForUs.People.fetchAll({
+  let peopleHistory = await TheyWorkForUs.People.fetchAll({
     fields: ['Name', 'PeoplePartyHistory'],
     'nested[PeoplePartyHistory][nested][Party][fields]': 'Id,Name,PartyGroup',
   })
@@ -12,28 +12,25 @@ export const getPeople = async (province, zone) => {
     fields: ['Id', 'Name', 'Images'],
   })
   // filter candidates by province and zone
-  let peoples = peoples_list
-  peoples = peoples.filter(
-    (people) => people.Province == province && people.Zone == zone
+  let people = await people_list
+  people = await people.filter(
+    (person) => person.Province == province && person.Zone == zone
   )
-  // left join between candidate peoplePartyHistory
-  peoples = peoples.map((people) =>
-    peoplesHistory.some((history) => people.Name === history.Name)
-      ? Object.assign(
-          people,
-          peoplesHistory.find((history) => history.Name === people.Name)
-        )
-      : people
-  )
-  // join candidates and party (for use image party)
-  for (let i = 0; i < peoples.length; i++) {
-    let partyName = await peoples[i].Party
-    peoples[i]['Party'] = await parties.find(
-      (party) => party.Name === partyName
-    )
-  }
 
-  return peoples
+  return people.map((person_) => {
+    let person = {...person_}
+    const partyFromApi = parties.find((party) => party.Name === person.Party)
+    if (partyFromApi) person.Party = partyFromApi
+    else person.Party = { Name: person.Party, Image: '' }
+    person = peopleHistory.some((history) => person.Name === history.Name)
+      ? Object.assign(
+          person,
+          peopleHistory.find((history) => history.Name === person.Name)
+        )
+      : person
+
+    return person
+  })
 }
 
 export const getParties = async (province, zone) => {
