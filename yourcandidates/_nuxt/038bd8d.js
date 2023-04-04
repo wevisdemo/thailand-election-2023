1cf9ebeb2b22186350bf196f2f7b71aebdff8ca1
JaeAiKay <78583537+JaeAiKay@users.noreply.github.com>
2023-04-04 09:57:42 +0000

    Deploying to gh-pages from @ wevisdemo/thailand-election-2023@5a8e5e99d456eb081b70e37e9d0367a07143284f 🚀

--- yourcandidates/_nuxt/038bd8d.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[36],{588:function(n,e,d){"use strict";d.r(e);var l=d(56),component=Object(l.a)({},undefined,undefined,!1,null,null,null);e.default=component.exports}}]);
