2befb3d24c6377a556f734c5c13964807c5ab10e
tesol2y090 <gliese090@gmail.com>
2023-04-09 22:39:48 +0700

    feat(electionsim): add spin animation to calculate

--- apps/electionsim/src/components/analyze/calculate.svelte
+++ apps/electionsim/src/components/analyze/calculate.svelte
@@ -14,31 +14,33 @@
 	class="text-center flex flex-col items-center justify-center w-[312px] md:w-[650px] h-full py-10"
 >
 	<svg
+		class="animate-spin"
 		width="69"
-		height="69"
-		viewBox="0 0 69 69"
+		height="70"
+		viewBox="0 0 69 70"
 		fill="none"
 		xmlns="http://www.w3.org/2000/svg"
 	>
 		<path
-			d="M66.5138 40.0567C65.4148 46.2896 62.4922 52.0566 58.1156 56.6284C53.7391 61.2003 48.105 64.3716 41.9261 65.7415C35.7471 67.1113 29.3007 66.6181 23.402 64.3243C17.5033 62.0304 12.4173 58.0389 8.78713 52.8544C5.15697 47.67 3.14568 41.5256 3.00762 35.1981C2.86955 28.8706 4.6109 22.6442 8.01147 17.3064C11.412 11.9686 16.3191 7.75902 22.1121 5.21003C27.9051 2.66105 34.3239 1.88713 40.5567 2.98615"
-			stroke="url(#paint0_angular_1691_66021)"
+			d="M66.5138 40.5567C65.4148 46.7896 62.4922 52.5566 58.1156 57.1284C53.7391 61.7003 48.105 64.8716 41.9261 66.2415C35.7471 67.6113 29.3007 67.1181 23.402 64.8243C17.5033 62.5304 12.4173 58.5389 8.78713 53.3544C5.15697 48.17 3.14568 42.0256 3.00762 35.6981C2.86955 29.3706 4.6109 23.1442 8.01147 17.8064C11.412 12.4686 16.3191 8.25902 22.1121 5.71003C27.9051 3.16105 34.3239 2.38713 40.5567 3.48615"
+			stroke="url(#paint0_angular_1646_55418)"
 			stroke-width="5"
 		/>
 		<defs>
 			<radialGradient
-				id="paint0_angular_1691_66021"
+				id="paint0_angular_1646_55418"
 				cx="0"
 				cy="0"
 				r="1"
 				gradientUnits="userSpaceOnUse"
-				gradientTransform="translate(35 34.5) rotate(0.830315) scale(34.5036)"
+				gradientTransform="translate(35 35) rotate(0.830315) scale(34.5036)"
 			>
 				<stop stop-color="#FF0000" />
 				<stop offset="0.744792" stop-color="#0000FF" />
 			</radialGradient>
 		</defs>
 	</svg>
+
 	<h6 class="typo-h6 font-bold mt-5">กำลังคำนวณ</h6>
 	<h4 class="typo-b4 mt-1">มาดูแบบจำลองผล #เลือกตั้ง66จากคำตอบของคุณกัน</h4>
 	<div class="w-full bg-gray-4 mt-[180px] text-left">
