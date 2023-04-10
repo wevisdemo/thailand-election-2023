f1414fc7060510a543eee172d22f5564bcb4e2f2
Bariyard <worawitrr@gmail.com>
2023-04-10 07:26:21 +0000

    Deploying to gh-pages from @ wevisdemo/thailand-election-2023@78af024cee86fc4cab037c0b8f5b68e5d59f7965 🚀

--- mpasset/_next/static/E6Ox2mPbHr5av6snv20Kj/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-27e89144192fbe9f.js"],"/_error":["static/chunks/pages/_error-15a0bbaa3c2350f1.js"],"/about":["static/chunks/pages/about-bfc5b607aa127174.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
