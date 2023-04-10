3288a146ed76a7618eb7fd28120bb580cbeeae93
bariyard <worawitrr@gmail.com>
2023-04-10 14:08:44 +0700

    feat(mpasset) ui

---
+++ apps/mpasset/models/person.tsx
@@ -0,0 +1,25 @@
+﻿import { NestedPeoplePartyHistory } from "@thailand-election-2023/database";
+import { ImageAttachment } from "@thailand-election-2023/database/dist/models/image";
+
+export interface PersonCustom {
+  Id: number;
+  Title: string;
+  Name: string;
+  IsMp: boolean;
+  IsSenator: boolean;
+  IsActive: boolean;
+
+  // custom field
+  Images: ImageAttachment[] | string | null;
+  PeoplePartyHistory?: NestedPeoplePartyHistory[];
+  Party: {
+    Id: number;
+    Name: string;
+    Color?: string | null;
+    Images?: string | null;
+  } | null;
+  countDirector: number;
+  countCompShare: number;
+  totalValueShare: number;
+  totalPctShare: number;
+}
