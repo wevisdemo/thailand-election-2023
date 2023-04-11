65d1a0f3286576a0ecefe1702684a3ba176cdb44
napatswift <napat.do@ku.th>
2023-04-11 12:49:02 +0700

    feat(yourcandidates): 66 electoral districts data

--- apps/yourcandidates/nuxt.config.js
+++ apps/yourcandidates/nuxt.config.js
@@ -19,7 +19,13 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: 'https://wevis.info/wp-content/uploads/2022/01/favicon.png',
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
