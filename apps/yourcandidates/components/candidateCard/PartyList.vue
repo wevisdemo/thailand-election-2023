2932c13def2da8f30678f43ef78ff7e3db894a25
JaeAiKay <78583537+JaeAiKay@users.noreply.github.com>
2023-04-14 00:52:27 +0700

    feat(yourcandidates): add link view party list information #12

--- apps/yourcandidates/components/candidateCard/PartyList.vue
+++ apps/yourcandidates/components/candidateCard/PartyList.vue
@@ -11,21 +11,32 @@
       <div class="w-full relative">
         <slot name="close"></slot>
       </div>
-      <div class="flex flex-col items-start pt-4">
+      <div class="flex items-start pt-4">
         <b>บัญชีรายชื่อพรรค{{ nameParty }}</b>
+      </div>
+      <div>
+        <p style="display: inline-flex; background-color: var(--color-highlight-1); border-radius: 5px; padding: 0px 5px;">ไฮไลท์</p>
+        &nbsp;= เคยมีตำแหน่งในสภาสมัยที่แล้ว
       </div>
       <table class="w-full">
         <tr class="col-name">
-          <th style="table-layout: fixed; width: 1%">ลำดับ</th>
-          <th style="padding-left: 14px">ชื่อ</th>
+          <th style="table-layout: fixed; width: 1%;">ลำดับ</th>
+          <th style="padding: 5px 0px 5px 14px">ชื่อ</th>
         </tr>
         <tr
           v-for="(item, index) in partyList"
           :key="index"
           class="party-list-box"
+          :class="item.PastMP? 'bg-[color:var(--color-highlight-1)]':''"
+          @click="clickItem(item)"
         >
-          <td>{{ index }}</td>
-          <td style="padding-left: 14px">{{ item.Name }}</td>
+          <td style="padding: 5px 0px 5px 5px;">{{ index+1 }}</td>
+          <td style="padding: 5px 0px 5px 14px">{{ item.Name }}</td>
+          <td>
+            <svg width="10" height="8" viewBox="0 0 10 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M2.21061 6.80589C3.29779 5.35632 4.611 4.10436 5.9807 2.91998C6.04948 2.8605 7.69138 1.38616 7.63699 1.31002C7.56381 1.20757 6.74274 1.25395 6.69592 1.2521C5.8436 1.21859 4.96897 0.813484 4.11591 0.806034C3.27541 0.798693 2.44614 0.832917 1.60831 0.855259C1.53779 0.85714 1.26643 0.919996 1.4809 0.90738C2.51756 0.8464 3.61071 0.806034 4.64788 0.806034C5.14788 0.806034 6.04594 0.807319 6.69592 0.7351C7.25299 0.600634 8.1166 0.636099 8.67361 0.505036C8.91922 0.447247 8.69063 0.903584 8.67362 1.01466C8.4982 2.16005 8.31618 3.2541 8.03079 4.37936C7.80477 5.27055 7.52696 6.09503 7.52696 7.01437" stroke="#9A948C" stroke-linecap="round"/>
+            </svg>
+          </td>
         </tr>
       </table>
     </div>
@@ -46,6 +57,10 @@ export default {
     closePopup() {
       this.$emit('closePopup')
     },
+    clickItem(item){
+      const path = item.PastMP?'https://theyworkforus.wevis.info/people/' + item.Name.replaceAll(' ', '-') :'https://www.google.com/search?q=' + item.Name
+      window.open(path, '_blank');
+    }
   },
 }
 </script>
@@ -76,6 +91,7 @@ export default {
   box-sizing: border-box;
   text-align: start;
   border-bottom: 1px solid var(--color-gray-2);
+  cursor: pointer;
 }
 .scrollbar::-webkit-scrollbar {
   width: 5px;
@@ -88,7 +104,6 @@ export default {
 .scrollbar::-webkit-scrollbar-track {
   position: absolute;
   background: transparent;
-
 }
 
 .scrollbar::-webkit-scrollbar-thumb {
