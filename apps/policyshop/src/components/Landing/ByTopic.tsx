9a4c72f06f389289e98c5c6cd685557c2837ffb2
petchill <songpon111333@gmail.com>
2023-04-19 21:22:03 +0700

    feat(policyshop): add icon

--- apps/policyshop/src/components/Landing/ByTopic.tsx
+++ apps/policyshop/src/components/Landing/ByTopic.tsx
@@ -3,8 +3,13 @@ import Link from 'next/link';
 import { ByTopicProps } from '@/types/components';
 import CircleTopicWrapper from '../CircleTopicWrapper';
 import SearchBar from '../SearchBar';
+import { TopicType, getMediumTopicIconUrl } from '@/utils/data';
 
 const ByTopic: FC<ByTopicProps> = ({ topics }) => {
+	const getTopicImgSrc = (topic: string) => {
+		return getMediumTopicIconUrl(topic as TopicType);
+	};
+
 	return (
 		<div className="container relative pt-20 mx-auto" id="ByTopic">
 			<p className="px-4 py-2 font-bold typo-h6 bg-highlight-3">
@@ -17,7 +22,9 @@ const ByTopic: FC<ByTopicProps> = ({ topics }) => {
 						href={`/list?topic=${key}`}
 						className="mt-4 mx-[8px] md:mx-[10px]"
 					>
-						<CircleTopicWrapper name={key} totalTopic={topics[key].length} />
+						<CircleTopicWrapper name={key} totalTopic={topics[key].length}>
+							<img src={getTopicImgSrc(key)} alt={`topic-icon-${key}`} />
+						</CircleTopicWrapper>
 					</Link>
 				))}
 			</div>
--- apps/policyshop/src/utils/data.ts
+++ apps/policyshop/src/utils/data.ts
@@ -4,3 +4,96 @@ export const shufflePolicies = (policies: Policy[]): Policy[] => {
 	const result = policies.sort(() => Math.random() - 0.5);
 	return result;
 };
+
+type TopicMapIcon = {
+	[key in TopicType]: string;
+};
+
+type HotTopicMapIcon = {
+	[key in HotTopicType]: string;
+};
+
+export const hotTopics = [
+	'คอร์รัปชัน',
+	'เศรษฐกิจ',
+	'การศึกษา',
+	'ต่างประเทศ',
+	'การบริหารราชการ',
+	'ความมั่นคง',
+];
+
+export type HotTopicType =
+	| 'คอร์รัปชัน'
+	| 'เศรษฐกิจ'
+	| 'การศึกษา'
+	| 'ต่างประเทศ'
+	| 'การบริหารราชการ'
+	| 'ความมั่นคง';
+
+export type TopicType =
+	| 'ความมั่นคง'
+	| 'ความเท่าเทียม'
+	| 'การบริหารราชการ'
+	| 'ต่างประเทศ'
+	| 'คอร์รัปชัน'
+	| 'การศึกษา'
+	| 'แรงงาน'
+	| 'สิ่งแวดล้อม'
+	| 'เศรษฐกิจ'
+	| 'การแพทย์'
+	| 'อื่นๆ'
+	| 'วัฒนธรรม';
+
+export const getSmallTopicIconUrl = (key: TopicType) => {
+	const dict: TopicMapIcon = {
+		ความมั่นคง: '/icon/topic/security-small.svg',
+		ความเท่าเทียม: '/icon/topic/equality-small.svg',
+		การบริหารราชการ: '/icon/topic/management-small.svg',
+		ต่างประเทศ: '/icon/topic/foreign-small.svg',
+		คอร์รัปชัน: '/icon/topic/corruption-small.svg',
+		การศึกษา: '/icon/topic/education-small.svg',
+		แรงงาน: '/icon/topic/worker-small.svg',
+		สิ่งแวดล้อม: '/icon/topic/environment-small.svg',
+		เศรษฐกิจ: '/icon/topic/economy-small.svg',
+		การแพทย์: '/icon/topic/health-small.svg',
+		อื่นๆ: '/icon/topic/others-small.svg',
+		วัฒนธรรม: '/icon/topic/curlture-small.svg',
+	};
+	return dict[key];
+};
+
+export const getMediumTopicIconUrl = (key: TopicType) => {
+	const dict: TopicMapIcon = {
+		ความมั่นคง: '/icon/topic/security-medium.svg',
+		ความเท่าเทียม: '/icon/topic/equality-medium.svg',
+		การบริหารราชการ: '/icon/topic/management-medium.svg',
+		ต่างประเทศ: '/icon/topic/foreign-medium.svg',
+		คอร์รัปชัน: '/icon/topic/corruption-medium.svg',
+		การศึกษา: '/icon/topic/education-medium.svg',
+		แรงงาน: '/icon/topic/worker-medium.svg',
+		สิ่งแวดล้อม: '/icon/topic/environment-medium.svg',
+		เศรษฐกิจ: '/icon/topic/economy-medium.svg',
+		การแพทย์: '/icon/topic/health-medium.svg',
+		อื่นๆ: '/icon/topic/others-medium.svg',
+		วัฒนธรรม: '/icon/topic/curlture-medium.svg',
+	};
+	return dict[key];
+};
+
+export const getBigTopicIconUrl = (key: HotTopicType) => {
+	const dict: TopicMapIcon = {
+		ความมั่นคง: '/icon/topic/security-medium.svg',
+		ความเท่าเทียม: '/icon/topic/equality-medium.svg',
+		การบริหารราชการ: '/icon/topic/management-medium.svg',
+		ต่างประเทศ: '/icon/topic/foreign-medium.svg',
+		คอร์รัปชัน: '/icon/topic/corruption-medium.svg',
+		การศึกษา: '/icon/topic/education-medium.svg',
+		แรงงาน: '/icon/topic/worker-medium.svg',
+		สิ่งแวดล้อม: '/icon/topic/environment-medium.svg',
+		เศรษฐกิจ: '/icon/topic/economy-medium.svg',
+		การแพทย์: '/icon/topic/health-medium.svg',
+		อื่นๆ: '/icon/topic/others-medium.svg',
+		วัฒนธรรม: '/icon/topic/curlture-medium.svg',
+	};
+	return dict[key];
+};
