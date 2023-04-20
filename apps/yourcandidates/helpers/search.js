df652d2ae145c30f28b96b0bc1f06d6f216c5c21
napatswift <napat.do@ku.th>
2023-04-20 19:21:59 +0700

    fix(yourcandidates): spliter

--- apps/yourcandidates/helpers/search.js
+++ apps/yourcandidates/helpers/search.js
@@ -25,15 +25,11 @@ const HIGHTLIGHT_STYLE = {
 
 export const searchDistrict = (query) => {
   // split query by space when before space is not .
-  const queries = normalizeSearchQuery(query).split(
-    /^(?:\s|\.)*(.*?)(?:\s|\.)+$/g
-  )
+  const queries = normalizeSearchQuery(query).split(/\s/g)
   const districts = DISTRICT_LIST.map((district) => {
     let score = 0
     let matchedIndices = []
-    const stringMenuList = district.stringMenu.split(
-      /^(?:\s|\.)*(.*?)(?:\s|\.)+$/g
-    )
+    const stringMenuList = district.stringMenu.split(/\s/g)
     for (let index = 0; index < queries.length; index++) {
       const query = queries[index]
       let currIndex = 0
