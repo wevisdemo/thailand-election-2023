bd54c2ecb3c9db5921c472a9d9143df466214c6f
Tae Prasongpongchai <taepras@gmail.com>
2023-04-22 12:51:20 +0700

    fix(electionsim) minor adjust layout for analytic & recalaulate pages

--- apps/electionsim/src/components/preprocessing/analytic.svelte
+++ apps/electionsim/src/components/preprocessing/analytic.svelte
@@ -6,44 +6,52 @@
 <div
 	class="grow justify-between flex flex-col items-center w-full max-w-[698px] px-6 py-0 md:py-10"
 >
-	<h4 class="typo-h4 beyonx-text-gradient text-center">
-		<b>
-			วิเคราะห์ผลเลือกตั้ง <br />ต้องดูจากอะไรบ้าง?
-		</b>
-	</h4>
-	<p class="typo-b4 text-center my-5">
-		4 ปีนับจาก #เลือกตั้ง62 ปัจจัยหลายอย่างในการเมืองไทยเปลี่ยนไป
-		ทั้งกติกาเลือกตั้ง กลยุทธ์ของพรรคต่างๆ และกระแสความนิยมของประชาชน
-		<b>ปัจจัยเหล่านี้ล้วนมีผลต่อผลการเลือกตั้ง 66 ที่กำลังจะเกิดขึ้น</b>
-		<br />
-		<br />
-		WeVis และ Beyond X ได้ลองนำปัจจัยเบื้องต้นเหล่านี้มาเป็นข้อสันนิษฐานใน
-		<b>เครื่องมือทดลองวิเคราะห์ผล #เลือกตั้ง66</b> ชิ้นนี้ เพื่อให้ทุกคนค่อยๆ ทำความเข้าใจไปด้วยกัน
-	</p>
-	<div class="w-full bg-gray-4">
-		<p class="typo-b6 py-2 px-4">
-			<b>Disclaimer</b>: ผลการวิเคราะห์นี้เป็นเพียงการทดลองประมาณการแบบคร่าวๆ
-			บนฐานข้อมูลผลการเลือกตั้งเดิมบนกติกาเลือกตั้งใหม่และข้อสันนิษฐานที่กำหนดไว้เท่านั้น
-			อาจมีปัจจัยอื่นที่ส่งผลต่อผลลัพธ์ในการเลือกตั้ง
-			และไม่ได้มีเจตนาในการชี้นำผลการเลือกตั้งแต่อย่างใด
+	<div class="grow flex flex-col justify-center items-center pb-10">
+		<h4 class="typo-h4 beyonx-text-gradient text-center">
+			<b>
+				วิเคราะห์ผลเลือกตั้ง <br />ต้องดูจากอะไรบ้าง?
+			</b>
+		</h4>
+		<p class="typo-b4 text-center my-5">
+			4 ปีนับจาก #เลือกตั้ง62 ปัจจัยหลายอย่างในการเมืองไทยเปลี่ยนไป
+			ทั้งกติกาเลือกตั้ง กลยุทธ์ของพรรคต่างๆ และกระแสความนิยมของประชาชน
+			<b>ปัจจัยเหล่านี้ล้วนมีผลต่อผลการเลือกตั้ง 66 ที่กำลังจะเกิดขึ้น</b>
+		</p>
+		<p class="typo-b4 text-center">
+			WeVis และ Beyond X ได้ลองนำปัจจัยเบื้องต้นเหล่านี้มาเป็นข้อสันนิษฐานใน
+			<b>เครื่องมือทดลองวิเคราะห์ผล #เลือกตั้ง66</b> ชิ้นนี้ เพื่อให้ทุกคนค่อยๆ ทำความเข้าใจไปด้วยกัน
 		</p>
 	</div>
-	<div class="w-full mt-10 flex items-center justify-between space-x-5">
-		<a
-			class="typo-b3 px-4 py-2 border flex items-center justify-between border-black w-full"
-			href={base}
-		>
-			<div class="border-1 border-t border-l w-2 h-2 -rotate-45 border-black" />
-			<b>ย้อนกลับ</b>
-		</a>
-		<button
-			class="typo-b3 px-4 py-2 border flex items-center justify-between border-black w-full text-white bg-black"
-			on:click={() => {
-				contentManager.updateContent(Content.Election62);
-			}}
-		>
-			<b>ต่อไป</b>
-			<div class="border-1 border-t border-r w-2 h-2 rotate-45 border-white" />
-		</button>
+	<div>
+		<div class="w-full bg-gray-4">
+			<p class="typo-b6 py-2 px-4">
+				<b>Disclaimer</b>: ผลการวิเคราะห์นี้เป็นเพียงการทดลองประมาณการแบบคร่าวๆ
+				บนฐานข้อมูลผลการเลือกตั้งเดิมบนกติกาเลือกตั้งใหม่และข้อสันนิษฐานที่กำหนดไว้เท่านั้น
+				อาจมีปัจจัยอื่นที่ส่งผลต่อผลลัพธ์ในการเลือกตั้ง
+				และไม่ได้มีเจตนาในการชี้นำผลการเลือกตั้งแต่อย่างใด
+			</p>
+		</div>
+		<div class="w-full mt-5 flex items-center justify-between space-x-5">
+			<a
+				class="typo-b3 px-4 py-2 border flex items-center justify-between border-black w-full"
+				href={base}
+			>
+				<div
+					class="border-1 border-t border-l w-2 h-2 -rotate-45 border-black"
+				/>
+				<b>ย้อนกลับ</b>
+			</a>
+			<button
+				class="typo-b3 px-4 py-2 border flex items-center justify-between border-black w-full text-white bg-black"
+				on:click={() => {
+					contentManager.updateContent(Content.Election62);
+				}}
+			>
+				<b>ต่อไป</b>
+				<div
+					class="border-1 border-t border-r w-2 h-2 rotate-45 border-white"
+				/>
+			</button>
+		</div>
 	</div>
 </div>
--- apps/electionsim/src/components/preprocessing/recalculate.svelte
+++ apps/electionsim/src/components/preprocessing/recalculate.svelte
@@ -5,47 +5,51 @@
 <div
 	class="grow justify-between flex flex-col items-center w-full max-w-[698px] px-6 py-0 md:py-10"
 >
-	<h4 class="typo-h4 beyonx-text-gradient text-center">
-		<b>
-			ปรับฐานการคำนวณแล้ว
-			<br class="hidden md:block" /> มาเริ่มวิเคราะห์กัน
-		</b>
-	</h4>
-	<div class="my-10">
-		<p class="typo-b4 text-center">
-			เราจะให้คุณ <b>ลองตอบคำถาม 7 ข้อ</b> เพื่อนำข้อมูลมาคำนวณเป็นแบบจำลองหนึ่งของผลการเลือกตั้ง
-			66
-		</p>
-		<p class="typo-b4 text-center mt-5">
-			ถ้าไม่แน่ใจข้อไหน ไม่เป็นไร ลองเดาได้ สามารถกลับมาแก้ไขคำตอบภายหลังได้
-		</p>
+	<div class="grow flex flex-col items-center justify-center">
+		<h4 class="typo-h4 beyonx-text-gradient text-center">
+			<b>
+				ปรับฐานการคำนวณแล้ว
+				<br class="hidden md:block" /> มาเริ่มวิเคราะห์กัน
+			</b>
+		</h4>
+		<div class="mt-5">
+			<p class="typo-b4 text-center">
+				เราจะให้คุณ <b>ลองตอบคำถาม 7 ข้อ</b> เพื่อนำข้อมูลมาคำนวณเป็นแบบจำลองหนึ่งของผลการเลือกตั้ง
+				66
+			</p>
+			<p class="typo-b4 text-center mt-5">
+				ถ้าไม่แน่ใจข้อไหน ไม่เป็นไร ลองเดาได้ สามารถกลับมาแก้ไขคำตอบภายหลังได้
+			</p>
+		</div>
 	</div>
-	<div class="w-full bg-gray-4">
-		<p class="typo-b6 py-2 px-4">
-			<b>Disclaimer</b>: ผลการวิเคราะห์นี้เป็นเพียงการทดลองประมาณการแบบคร่าวๆ
-			บนฐานข้อมูลผลการเลือกตั้งเดิมบนกติกาเลือกตั้งใหม่และข้อสันนิษฐานที่กำหนดไว้เท่านั้น
-			อาจมีปัจจัยอื่นที่ส่งผลต่อผลลัพธ์ในการเลือกตั้ง
-			และไม่ได้มีเจตนาในการชี้นำผลการเลือกตั้งแต่อย่างใด
-		</p>
+	<div class="flex flex-col items-center mt-10">
+		<div class="w-full bg-gray-4">
+			<p class="typo-b6 py-2 px-4">
+				<b>Disclaimer</b>: ผลการวิเคราะห์นี้เป็นเพียงการทดลองประมาณการแบบคร่าวๆ
+				บนฐานข้อมูลผลการเลือกตั้งเดิมบนกติกาเลือกตั้งใหม่และข้อสันนิษฐานที่กำหนดไว้เท่านั้น
+				อาจมีปัจจัยอื่นที่ส่งผลต่อผลลัพธ์ในการเลือกตั้ง
+				และไม่ได้มีเจตนาในการชี้นำผลการเลือกตั้งแต่อย่างใด
+			</p>
+		</div>
+		<button
+			class="typo-b3 beyondx-gradient-bg text-white py-2 px-4 w-[160px] md:w-52 h-[50px] flex items-center justify-between font-bold mt-5"
+			on:click={() => {
+				contentManager.updateContent(Content.Quiz1);
+			}}
+		>
+			เริ่มวิเคราะห์
+			<div class="border-1 border-t border-r w-2 h-2 rotate-45" />
+		</button>
+		<button
+			class="typo-b3 py-2 px-4 w-[312px] flex items-center justify-center font-bold mt-5 text-byx-blue"
+			on:click={() => {
+				contentManager.updateContent(Content.Election62);
+			}}
+		>
+			<div
+				class="border-1 border-t border-l w-2 h-2 -rotate-45 border-byx-blue mr-2"
+			/>
+			ย้อนกลับ
+		</button>
 	</div>
-	<button
-		class="typo-b3 beyondx-gradient-bg text-white py-2 px-4 w-[160px] md:w-52 h-[50px] flex items-center justify-between font-bold mt-10"
-		on:click={() => {
-			contentManager.updateContent(Content.Quiz1);
-		}}
-	>
-		เริ่มวิเคราะห์
-		<div class="border-1 border-t border-r w-2 h-2 rotate-45" />
-	</button>
-	<button
-		class="typo-b3 py-2 px-4 w-[312px] flex items-center justify-center font-bold mt-2 text-byx-blue"
-		on:click={() => {
-			contentManager.updateContent(Content.Election62);
-		}}
-	>
-		<div
-			class="border-1 border-t border-l w-2 h-2 -rotate-45 border-byx-blue mr-2"
-		/>
-		ย้อนกลับ
-	</button>
 </div>
