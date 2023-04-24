8540937238afdc925e054536bb1fe00a7dd78fe5
petchill <songpon111333@gmail.com>
2023-04-24 23:05:54 +0700

    feat(policyshop): cookie tag

--- apps/policyshop/src/pages/_app.tsx
+++ apps/policyshop/src/pages/_app.tsx
@@ -3,8 +3,13 @@ import '@thailand-election-2023/styles/dist/index.css';
 import type { AppProps } from 'next/app';
 import SafeHydrate from '../components/SafeHydrate';
 import Metadata from '@/components/Metadata';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
+	useEffect(() => {
+		// @ts-ignore
+		import('@thailand-election-2023/components');
+	});
 	return (
 		<>
 			<Metadata />
