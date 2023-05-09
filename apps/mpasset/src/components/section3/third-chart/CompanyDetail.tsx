16ace81e041a51fc44b35018e9f1dd2823cce3f4
bariyard <worawitrr@gmail.com>
2023-05-09 16:43:25 +0700

    fix(mpasset) increase clickable area

--- apps/mpasset/src/components/section3/third-chart/CompanyDetail.tsx
+++ apps/mpasset/src/components/section3/third-chart/CompanyDetail.tsx
@@ -74,13 +74,12 @@ const CompanyDetail = ({ open, onToggle }: Props) => {
       pb-[100px]
       max-w-[480px]
       `}>
-      <div className='flex flex-row justify-between items-center mb-[10.5px]'>
+      <div className='flex flex-row justify-between items-center mb-[10.5px] cursor-pointer desktop:cursor-default' onClick={() => onToggle()}>
         <div className='flex-shrink truncate typo-h7 font-bold text-kondolar leadin-[140%]'>{selectedCompany?.company_name_th}</div>
         <div className='flex-grow desktop:hidden'>
           <div className='flex flex-row flex-nowrap justify-end gap-x-[9px]'>
             <div className='whitespace-nowrap'>สองเพิ่มเติม</div>
             <svg className='cursor-pointer'
-              onClick={() => onToggle()}
               width={18} height={21} viewBox="0 0 18 21" fill="none" xmlns="http://www.w3.org/2000/svg">
               <g transform={!open ? `` : `rotate(180 9 10.5)`}>
                 <path d="M8.99477 19.5C8.99477 18.6288 9.53505 15.367 9.45186 14.5C9.31774 13.1021 8.82178 12.4134 8.80946 11C8.78908 8.66088 8.75586 8.09703 8.87741 5.77256C8.90547 5.23592 8.93918 5.00754 8.93918 4.5C8.40451 5.0132 7.74585 4.86917 7.21583 5.38947C5.23111 7.33779 -0.544392 13.3727 1.39104 11.3721C2.83864 9.87574 4.19694 8.2418 5.58514 6.6856C6.75868 5.37002 7.89038 3.91503 8.80946 2.39497C9.02275 2.04222 9.16673 1.15737 9.45186 1.64156C10.253 3.00195 11.0273 4.33533 11.9597 5.60656C13.0568 7.10229 14.1073 8.64607 15.1716 10.1653C15.2557 10.2853 15.3779 10.4604 15.5052 10.5357C16.0965 10.8849 16.4117 11.7257 17 12.0297" stroke="#090909" strokeWidth={2} strokeLinecap="round" />
