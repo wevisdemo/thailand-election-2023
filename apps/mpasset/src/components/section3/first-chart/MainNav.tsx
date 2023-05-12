d0ee1ad9c896585029776c1ae7f421d758e374e1
Th1nkK1D <witheep@gmail.com>
2023-05-12 03:08:01 +0300

    refactor(mpasset): use declarative instead of d3 select in MainNav

--- apps/mpasset/src/components/section3/first-chart/MainNav.tsx
+++ apps/mpasset/src/components/section3/first-chart/MainNav.tsx
@@ -1,9 +1,16 @@
 import { useTour } from '@reactour/tour';
 import * as d3 from 'd3';
-import React from 'react';
+import { useCallback, useMemo, useRef } from 'react';
 import { PersonCustom } from '../../../models/person';
 import { usePersonStore } from '../../../store/person';
 import { convertToInternationalCurrencySystem } from '../../util/calculation';
+
+const AVATAR_SIZE = 400;
+const R = 30;
+const LIMITER = 25;
+const CIRCLE_MARGIN = 5;
+const R_LOGO = 10;
+const TOOLTIP_OFFSET = 10;
 
 type Props = {
 	width: number;
@@ -13,11 +20,10 @@ type Props = {
 
 const MainNav = ({ width, height, filteredPerson }: Props) => {
 	const { setSelectedPerson, party } = usePersonStore();
-
-	// tour
 	const { currentStep, setCurrentStep } = useTour();
+	const tooltipRef = useRef<HTMLDivElement>(null);
 
-	const handleClick = React.useCallback(
+	const handleClick = useCallback(
 		(d: PersonCustom) => {
 			const p = party.find((p) => p.Name === d.PartyName);
 			if (p)
@@ -39,253 +45,237 @@ const MainNav = ({ width, height, filteredPerson }: Props) => {
 		[currentStep, party, setCurrentStep, setSelectedPerson]
 	);
 
-	React.useMemo(() => {
-		if (filteredPerson.length > 0) {
-			const data = filteredPerson.filter(
-				(d) => d.countCompShare > 0 || d.countDirector > 0
-			);
+	const { data, contentHeight, yScaleBand } = useMemo(() => {
+		const data = filteredPerson.filter(
+			(d) => d.countCompShare > 0 || d.countDirector > 0
+		);
+		const countPerson = data.length + 2;
 
-			const countPerson = data.length + 2;
-			const contentHeight = countPerson * 40;
-
-			const svg = d3
-				.select('.chart-main')
-				.attr('width', width)
-				.attr('height', Math.max(contentHeight, height));
-
-			const yScaleBand = d3
+		return {
+			data,
+			contentHeight: countPerson * 40,
+			yScaleBand: d3
 				.scaleBand()
 				.domain(data.map((_, i) => `${i}`))
 				.range([0, 40 * countPerson])
 				.paddingInner(0.5)
-				.paddingOuter(0.2);
+				.paddingOuter(0.2),
+		};
+	}, [filteredPerson]);
 
-			const xScale = d3.scaleLinear().domain([-10, 30]).range([0, width]);
+	const { xScale, getWidth } = useMemo(() => {
+		const xScale = d3.scaleLinear().domain([-10, 30]).range([0, width]);
 
-			const avatar_size = 400;
-			svg.selectAll('defs').remove();
+		const getWidth = (totalPctShare: number) => {
+			const w =
+				totalPctShare <= 0
+					? xScale(0) - xScale(totalPctShare)
+					: xScale(totalPctShare) - xScale(0);
 
-			//tooltip
-			const tooltip = d3
-				.select('#tooltip-main-nav')
-				.style('position', 'absolute')
-				.style('visibility', 'hidden')
-				.style('background-color', 'white')
-				.style('border', 'solid')
-				.style('border-width', '1px')
-				.style('border-radius', '5px')
-				.style('padding', '10px');
+			return totalPctShare > 10 ? w : w;
+		};
 
-			const node = svg
-				.selectAll('g')
-				.data(data)
-				.join('g')
-				.attr('transform', (_, i) => `translate(0, ${yScaleBand(`${i!}`)})`)
-				.attr('class', `cursor-pointer`)
-				.on('click', (_, d: PersonCustom) => handleClick(d))
-				.on('mouseover', function (_, d) {
-					tooltip.style('visibility', 'visible');
-					tooltip.html(d.Name!);
-				})
-				.on('mousemove', function (e) {
-					return tooltip
-						.style('top', e.offsetY + 10 + 'px')
-						.style('left', e.offsetX + 10 + 'px');
-				})
-				.on('mouseout', function () {
-					return tooltip.style('visibility', 'hidden');
-				});
-
-			node.selectAll('rect').remove();
-			node.selectAll('text').remove();
-
-			const getWidth = (totalPctShare: number) => {
-				let w =
-					totalPctShare <= 0
-						? xScale(0) - xScale(totalPctShare)
-						: xScale(totalPctShare) - xScale(0);
-
-				// return w <= 0 ? 10 : w
-				return totalPctShare > 10 ? w : w;
-			};
-
-			node
-				.append('rect')
-				.attr('x', (d) =>
-					d.totalPctShare! < 0 ? xScale(d.totalPctShare!) : xScale(0)
-				)
-				.attr('y', 0)
-				.attr('width', (d) => getWidth(d.totalPctShare!))
-				.attr('height', (_) => yScaleBand.bandwidth())
-				.attr('fill', (d) =>
-					d.PartyName ? `url(#pattern_party_color_${d.PartyName})` : 'black'
-				);
-
-			node.selectAll('defs').remove();
-
-			const defs = node
-				.filter((d) => typeof d.Images === 'string' && d.Images !== '')
-				.append('svg:defs');
-			// profile
-			defs
-				.append('svg:pattern')
-				.attr(
-					'id',
-					(d) => 'pattern_person_avatar_' + d.Name.replaceAll(' ', '-')
-				)
-				.attr('width', 1)
-				.attr('height', 1)
-				// .attr("patternUnits", "userSpaceOnUse")
-				.attr('patternContentUnits', 'objectBoundingBox')
-				.append('use')
-				.attr('xlink:href', (_, i) => '#person_avatar' + i)
-				.attr('transform', 'scale(0.0025)');
-			defs
-				.append('svg:image')
-				.attr('id', (_, i) => 'person_avatar' + i)
-				.attr('xlink:href', (d) => String(d.Images))
-				.attr('width', avatar_size)
-				.attr('height', avatar_size)
-				.attr('x', 0)
-				.attr('y', 0);
-
-			const r = 30;
-			const limiter = 25;
-			node.selectAll('circle').remove();
-
-			let circleMargin = 5;
-
-			node
-				.append('rect')
-				.attr('x', (d) =>
-					d.totalPctShare < limiter
-						? d.totalPctShare < 0
-							? xScale(0) + r + circleMargin
-							: xScale(0) - r - circleMargin
-						: xScale(d.totalPctShare!) - (r + 2)
-				)
-				.attr('y', yScaleBand.bandwidth() - r + circleMargin)
-				.attr('rx', 49)
-				.attr('width', r)
-				.attr('height', r)
-				.attr('fill', (d) =>
-					typeof d.Images === 'string' && d.Images !== ''
-						? `url(#pattern_person_avatar_${d.Name.replaceAll(' ', '-')})`
-						: 'url(#pattern_person_avatar_default)'
-				)
-				.attr('stroke', 'black');
-
-			const rLogo = 10;
-			node
-				.filter((d) => typeof d.PartyName === 'string')
-				.append('rect')
-				.attr('x', (d) =>
-					d.totalPctShare! < limiter
-						? d.totalPctShare! < 0
-							? xScale(0) + r + circleMargin
-							: xScale(0) - r - circleMargin
-						: xScale(d.totalPctShare!) - r
-				)
-				.attr('y', yScaleBand.bandwidth() - rLogo * 0.5)
-				.attr('rx', 49)
-				.attr('width', rLogo)
-				.attr('height', rLogo)
-				.attr('fill', (d) =>
-					d.PartyName ? `url(#pattern_party_${d.PartyName})` : 'none'
-				);
-
-			node
-				.append('text')
-				.attr('x', (d) =>
-					d.totalPctShare! < limiter
-						? d.totalPctShare! < 0
-							? xScale(0) + r * 2 + circleMargin * 2
-							: xScale(d.totalPctShare!) + r * 0.5
-						: xScale(d.totalPctShare!) - r * 2 - circleMargin
-				)
-				.attr('y', yScaleBand.bandwidth() * 0.5)
-				.attr('rx', '2')
-				.attr('fill', (d) => (d.totalPctShare! < limiter ? 'black' : 'white'))
-				.attr('text-anchor', (d) =>
-					d.totalPctShare! < limiter ? 'start' : 'end'
-				)
-				.attr('alignment-baseline', 'middle')
-				.attr('class', 'typo-b7')
-				.text((d) =>
-					d.totalValueShare !== 0
-						? convertToInternationalCurrencySystem(d.totalValueShare!)
-						: d.countDirector > 0 || d.countCompShare > 0
-						? `เกี่ยวข้องกับธุรกิจแต่ไม่มีหุ้นอยู่`
-						: 'ไม่เกี่ยวข้องกับธุรกิจ'
-				);
-
-			// default unknown person
-			const defsDefault = svg.append('svg:defs');
-			defsDefault
-				.append('svg:pattern')
-				.attr('id', 'pattern_person_avatar_default')
-				.attr('width', 1)
-				.attr('height', 1)
-				.attr('patternContentUnits', 'objectBoundingBox')
-				.append('use')
-				.attr('xlink:href', '#person_avatar_default')
-				.attr('transform', 'scale(0.0025)');
-			defsDefault
-				.append('svg:image')
-				.attr('id', 'person_avatar_default')
-				.attr('xlink:href', '/mpasset/design_assets/profile_pic.jpg')
-				.attr('width', avatar_size)
-				.attr('height', avatar_size)
-				.attr('x', 0)
-				.attr('y', 0);
-
-			// all party image
-			const defsAllParty = defsDefault.selectAll(null).data(party);
-
-			const defsEnter = defsAllParty.enter();
-			defsEnter
-				.append('svg:pattern')
-				.attr('id', (d) => `pattern_party_${d.Name}`)
-				.attr('width', 1)
-				.attr('height', 1)
-				.attr('patternContentUnits', 'objectBoundingBox')
-				.append('use')
-				.attr('xlink:href', (d) => `#party_avatar_${d.Name}`)
-				.attr('transform', 'scale(0.0025)');
-
-			defsEnter
-				.append('svg:image')
-				.attr('id', (d) => `party_avatar_${d.Name}`)
-				.attr('xlink:href', (d) =>
-					d.Images ? String(`/mpasset/party/${d.Name}.webp`) : ''
-				)
-				.attr('width', avatar_size)
-				.attr('height', avatar_size)
-				.attr('x', 0)
-				.attr('y', 0);
-
-			defsEnter
-				.append('linearGradient')
-				.attr('id', (d) => `pattern_party_color_${d.Name}`)
-				.append('stop')
-				.attr('stop-color', (d) => d.Color);
-		}
-	}, [filteredPerson, height, width, party, handleClick]);
+		return {
+			xScale,
+			getWidth,
+		};
+	}, [width]);
 
 	return (
 		<div
 			className={`overflow-y-scroll overflow-x-hidden relative`}
 			style={{ maxHeight: `${height}px` }}
 		>
+			<div className="absolute top-0 inset-x-0 h-1/2 pointer-events-none tour1-first-step tour3-first-step" />
+			<svg
+				className="chart-main"
+				width={width}
+				height={Math.max(contentHeight, height)}
+			>
+				<defs>
+					<AvatarDefinition
+						patternId="pattern_person_avatar_default"
+						imageId="person_avatar_default"
+						imageHref="/mpasset/design_assets/profile_pic.jpg"
+					/>
+					{data
+						.filter((d) => typeof d.Images === 'string' && d.Images !== '')
+						.map((d, i) => (
+							<AvatarDefinition
+								key={d.Id}
+								patternId={
+									'pattern_person_avatar_' + d.Name.replaceAll(' ', '-')
+								}
+								imageId={'person_avatar' + i}
+								imageHref={`${d.Images}`}
+							/>
+						))}
+					{party.map((d) => (
+						<>
+							<AvatarDefinition
+								patternId={`pattern_party_${d.Name}`}
+								imageId={`#party_avatar_${d.Name}`}
+								imageHref={
+									d.Images ? String(`/mpasset/party/${d.Name}.webp`) : ''
+								}
+							/>
+							<linearGradient id={`pattern_party_color_${d.Name}`}>
+								<stop stopColor={d.Color || undefined}></stop>
+							</linearGradient>
+						</>
+					))}
+				</defs>
+
+				{data.map((d, i) => {
+					const isUnderLimit = d.totalPctShare < LIMITER;
+					const xZero = xScale(0);
+					const xShare = xScale(d.totalPctShare);
+					const ybandwidth = yScaleBand.bandwidth();
+
+					return (
+						<g
+							key={d.Id}
+							className="cursor-pointer"
+							transform={`translate(0, ${yScaleBand(`${i!}`)})`}
+							onClick={() => handleClick(d)}
+							onMouseOver={() => {
+								if (!tooltipRef.current) return;
+								tooltipRef.current.style.visibility = 'visible';
+								tooltipRef.current.innerText = d.Name;
+							}}
+							onMouseLeave={() => {
+								if (!tooltipRef.current) return;
+								tooltipRef.current.style.visibility = 'hidden';
+							}}
+							onMouseMove={({ nativeEvent: { offsetX, offsetY } }) => {
+								if (!tooltipRef.current) return;
+								tooltipRef.current.style.left = `${offsetX + TOOLTIP_OFFSET}px`;
+								tooltipRef.current.style.top = `${offsetY + TOOLTIP_OFFSET}px`;
+							}}
+						>
+							{/* bar chart */}
+							<rect
+								x={d.totalPctShare! < 0 ? xShare : xZero}
+								y="0"
+								width={getWidth(d.totalPctShare!)}
+								height={ybandwidth}
+								fill={
+									d.PartyName
+										? `url(#pattern_party_color_${d.PartyName})`
+										: 'black'
+								}
+							/>
+							{/* avatar */}
+							<rect
+								x={
+									isUnderLimit
+										? d.totalPctShare < 0
+											? xZero + R + CIRCLE_MARGIN
+											: xZero - R - CIRCLE_MARGIN
+										: xShare - (R + 2)
+								}
+								y={ybandwidth - R + CIRCLE_MARGIN}
+								rx="49"
+								width={R}
+								height={R}
+								fill={
+									typeof d.Images === 'string' && d.Images !== ''
+										? `url(#pattern_person_avatar_${d.Name.replaceAll(
+												' ',
+												'-'
+										  )})`
+										: 'url(#pattern_person_avatar_default)'
+								}
+								stroke="black"
+							/>
+							{/* party logo */}
+							<rect
+								x={
+									isUnderLimit
+										? d.totalPctShare! < 0
+											? xZero + R + CIRCLE_MARGIN
+											: xZero - R - CIRCLE_MARGIN
+										: xShare - R
+								}
+								y={ybandwidth - R_LOGO * 0.5}
+								rx="49"
+								width={R_LOGO}
+								height={R_LOGO}
+								fill={
+									d.PartyName ? `url(#pattern_party_${d.PartyName})` : 'none'
+								}
+							/>
+							{/* text */}
+							<text
+								x={
+									isUnderLimit
+										? d.totalPctShare! < 0
+											? xZero + R * 2 + CIRCLE_MARGIN * 2
+											: xShare + R * 0.5
+										: xShare - R * 2 - CIRCLE_MARGIN
+								}
+								y={ybandwidth * 0.5}
+								rx="2"
+								fill={d.totalPctShare! < LIMITER ? 'black' : 'white'}
+								textAnchor={d.totalPctShare! < LIMITER ? 'start' : 'end'}
+								alignmentBaseline="middle"
+								className="typo-b7"
+							>
+								{d.totalValueShare !== 0
+									? convertToInternationalCurrencySystem(d.totalValueShare!)
+									: d.countDirector > 0 || d.countCompShare > 0
+									? `เกี่ยวข้องกับธุรกิจแต่ไม่มีหุ้นอยู่`
+									: 'ไม่เกี่ยวข้องกับธุรกิจ'}
+							</text>
+						</g>
+					);
+				})}
+			</svg>
+
 			<div
-				className="absolute top-0 inset-x-0 h-1/2 pointer-events-none
-      tour1-first-step 
-      tour3-first-step"
-			/>
-			<svg className="chart-main" />
-			<div id="tooltip-main-nav" />
+				ref={tooltipRef}
+				style={{
+					position: 'absolute',
+					backgroundColor: 'white',
+					border: 'solid',
+					borderWidth: '1px',
+					borderRadius: '5px',
+					padding: '10px',
+					visibility: 'hidden',
+				}}
+			></div>
 		</div>
 	);
 };
+
+const AvatarDefinition = ({
+	patternId,
+	imageId,
+	imageHref,
+}: {
+	patternId: string;
+	imageId: string;
+	imageHref: string;
+}) => (
+	<>
+		<pattern
+			id={patternId}
+			width="1"
+			height="1"
+			patternContentUnits="objectBoundingBox"
+		>
+			<use xlinkHref={'#' + imageId}></use>
+		</pattern>
+		<image
+			id={imageId}
+			xlinkHref={imageHref}
+			transform="scale(0.0025)"
+			width={AVATAR_SIZE}
+			height={AVATAR_SIZE}
+			x="0"
+			y="0"
+		></image>
+	</>
+);
 
 export default MainNav;
