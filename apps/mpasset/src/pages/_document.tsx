d3bfb0cacc41a1347dae719ab57b4d6c46df79f2
Th1nkK1D <witheep@gmail.com>
2023-05-21 16:38:31 +0300

    feat(mpasset): update metadata

--- apps/mpasset/src/pages/_document.tsx
+++ apps/mpasset/src/pages/_document.tsx
@@ -1,32 +1,51 @@
 import { Html, Main, Head, NextScript } from 'next/document';
 
+const TITLE = 'นักการเมืองเกี่ยวข้องกับธุรกิจแค่ไหน?';
+const DESCRIPTION =
+	'ลองสืบ! คน(จะ)เป็นผู้แทนฯ มีธุรกิจอะไร เกี่ยวข้องกับโครงการรัฐไหนบ้าง';
+
 export default function Document() {
-  return (
-    <Html>
-      <Head>
-        <script
-          defer
-          data-domain={`${process.env.SECURE_HOST}${process.env.BASE_PATH}`}
-          src="https://analytics.punchup.world/js/plausible.js"
-        ></script>
-        <link rel="icon" href={`${process.env.SECURE_HOST}${process.env.BASE_PATH}/design_assets/sns/favicon.png`} />
-        <meta property="og:url" content={`${process.env.SECURE_HOST}${process.env.BASE_PATH}`} />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Mp Asset" />
-        <meta property="og:description" content="นักการเมืองเกี่ยวข้องกับธุรกิจแค่ไหน?" />
-        <meta property="og:image" content={`${process.env.SECURE_HOST}/design_assets/sns/og.jpg`} />
-        {/* twitter */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:url" content={`${process.env.SECURE_HOST}${process.env.BASE_PATH}`} />
-        <meta name="twitter:title" content="Mp Asset" />
-        <meta name="twitter:description" content="นักการเมืองเกี่ยวข้องกับธุรกิจแค่ไหน?" />
-        <meta name="twitter:image" content={`${process.env.SECURE_HOST}/design_assets/sns/og.jpg`} />
-      </Head>
-      <title>นักการเมืองเกี่ยวข้องกับธุรกิจแค่ไหน?</title>
-      <body className="scroll-smooth">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+	return (
+		<Html>
+			<Head>
+				<script
+					defer
+					data-domain={`${process.env.SECURE_HOST}${process.env.BASE_PATH}`}
+					src="https://analytics.punchup.world/js/plausible.js"
+				></script>
+				<link
+					rel="icon"
+					href={`${process.env.SECURE_HOST}${process.env.BASE_PATH}/design_assets/sns/favicon.png`}
+				/>
+				<meta
+					property="og:url"
+					content={`${process.env.SECURE_HOST}${process.env.BASE_PATH}`}
+				/>
+				<meta property="og:type" content="website" />
+				<meta property="og:title" content={TITLE} />
+				<meta property="og:description" content={DESCRIPTION} />
+				<meta
+					property="og:image"
+					content={`${process.env.SECURE_HOST}${process.env.BASE_PATH}/design_assets/sns/og.jpg`}
+				/>
+				{/* twitter */}
+				<meta name="twitter:card" content="summary_large_image" />
+				<meta
+					name="twitter:url"
+					content={`${process.env.SECURE_HOST}${process.env.BASE_PATH}`}
+				/>
+				<meta name="twitter:title" content={TITLE} />
+				<meta name="twitter:description" content={DESCRIPTION} />
+				<meta
+					name="twitter:image"
+					content={`${process.env.SECURE_HOST}/design_assets/sns/og.jpg`}
+				/>
+			</Head>
+			<title>{TITLE}</title>
+			<body className="scroll-smooth">
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
